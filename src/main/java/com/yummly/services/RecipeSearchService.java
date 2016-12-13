65403ac736397a1c925cf76b5d2557edd74fb45e
Carrie Ravotta <carrie.peary@gmail.com>
2016-12-13 14:08:37 -0500

    removed unused classes

--- src/main/java/com/yummly/services/RecipeSearchService.java
+++ src/main/java/com/yummly/services/RecipeSearchService.java
@@ -12,6 +12,7 @@ import okhttp3.HttpUrl;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 
 public class RecipeSearchService {
     public static final String TAG = RecipeSearchService.class.getSimpleName();
@@ -25,6 +26,24 @@ public class RecipeSearchService {
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+
+        //Yummly y = new Yummly("385fb868", "ba3cd23fe375d062943ece58724148ab");
+
+        SearchResult result;
+
+        Flavors minFlavors = new Flavors();
+        minFlavors.setPiquant(0.8);
+        ArrayList<NutritionRange> nutrition = new ArrayList<NutritionRange>();
+        NutritionRange range = new NutritionRange();
+        range.setMin(5);
+        range.setMax(100);
+        range.setNutrition(NutritionRange.NUTRITION.PROCNT);
+        nutrition.add(range);
+        //result = y.search("Chicken", true, null, null, -1, minFlavors, null, true, true, nutrition);
+
+        //Recipe recipe = y.getRecipe(result.getMatches().get(0).getId());
+        //System.out.println(String.format("%s, (%d), (%s) %s", recipe.getName(), recipe.getRating(), recipe.getId(), recipe.getImages().get(0).getHostedSmallUrl()));
     }
 
     public static Recipe getRecipeById(String id) throws IOException {
